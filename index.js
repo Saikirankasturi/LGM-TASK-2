1820d810c674adfe1b480ba97371fb7b4137075c
Saikirankasturi <86838339+Saikirankasturi@users.noreply.github.com>
2021-08-25 18:46:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <strong> Hi Saikiran,</strong> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
